de1bb73fb605841066cda9f071b5af7cdec9f5f9
Eduard Conesa Guerrero <eduard2207@hotmail.com>
2023-06-06 00:39:03 +0200

    Add batch normalization, droupout and rescale pixel values

---
+++ data_augmentation/train_conditional_gan.py
@@ -0,0 +1,220 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, BatchNormalization
+from tensorflow.keras import Model
+import time
+import sys
+sys.path.append('/tf/code/preprocess/data_loader')
+from data_loader import DataLoader
+
+loader = DataLoader()
+print(loader.length, loader.length_rois)
+
+batch_size = 4
+num_channels = 1
+num_classes = 8
+image_size = 256
+latent_dim = 128
+
+dataset = tf.data.Dataset.from_generator(loader.classification_generator(output_size=image_size,
+                                                                         classify_types=True,
+                                                                         types_as="hot_encode"
+                                                                        ),
+                                         output_signature=(
+                                             tf.TensorSpec(shape=(image_size,image_size,1), dtype=tf.float32, name="image"),
+                                             tf.TensorSpec(shape=(num_classes), dtype=tf.float32, name="label")
+                                         ),
+                                        )
+
+rescale_layer = tf.keras.layers.Rescaling(
+    scale=1./127.5, offset=-1
+)
+
+dataset = dataset.map(lambda x, y: (rescale_layer(x), y))
+
+train_ds = dataset.batch(batch_size)
+
+generator_in_channels = latent_dim + num_classes
+discriminator_in_channels = num_channels + num_classes
+print(generator_in_channels, discriminator_in_channels)
+
+# Create the discriminator.
+discriminator = tf.keras.Sequential(
+    [
+        tf.keras.layers.InputLayer((image_size, image_size, discriminator_in_channels)),
+        tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.LeakyReLU(alpha=0.2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.LeakyReLU(alpha=0.2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(256, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.LeakyReLU(alpha=0.2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Conv2D(512, (5, 5), strides=(1, 1), padding="same"),
+        tf.keras.layers.LeakyReLU(alpha=0.2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Conv2D(1024, (5, 5), strides=(1, 1), padding="same"),
+        tf.keras.layers.LeakyReLU(alpha=0.2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.GlobalMaxPooling2D(),
+        tf.keras.layers.Dense(1, activation="sigmoid"),
+    ],
+    name="discriminator",
+)
+
+# Create the generator.
+generator = tf.keras.Sequential(
+    [
+        tf.keras.layers.InputLayer((generator_in_channels,)),
+        # We want to generate 128 + num_classes coefficients to reshape into a
+        # 7x7x(128 + num_classes) map.
+        tf.keras.layers.Dense(4 * 4 * generator_in_channels),
+        tf.keras.layers.Reshape((4, 4, generator_in_channels)),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2DTranspose(1024, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2DTranspose(512, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2DTranspose(256, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding="same"),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(1, 1), padding="same"),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2DTranspose(32, (5, 5), padding="same"),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(1, (5, 5), padding="same", activation="tanh"),
+    ],
+    name="generator",
+)
+discriminator.summary()
+generator.summary()
+
+class ConditionalGAN(tf.keras.Model):
+    def __init__(self, discriminator, generator, latent_dim):
+        super().__init__()
+        self.discriminator = discriminator
+        self.generator = generator
+        self.latent_dim = latent_dim
+        self.gen_loss_tracker = tf.keras.metrics.Mean(name="generator_loss")
+        self.disc_loss_tracker = tf.keras.metrics.Mean(name="discriminator_loss")
+
+    @property
+    def metrics(self):
+        return [self.gen_loss_tracker, self.disc_loss_tracker]
+
+    def compile(self, d_optimizer, g_optimizer, loss_fn):
+        super().compile()
+        self.d_optimizer = d_optimizer
+        self.g_optimizer = g_optimizer
+        self.loss_fn = loss_fn
+
+    def train_step(self, data):
+        # Unpack the data.
+        real_images, one_hot_labels = data
+
+        # Add dummy dimensions to the labels so that they can be concatenated with
+        # the images. This is for the discriminator.
+        image_one_hot_labels = one_hot_labels[:, :, None, None]
+        image_one_hot_labels = tf.repeat(
+            image_one_hot_labels, repeats=[image_size * image_size]
+        )
+        image_one_hot_labels = tf.reshape(
+            image_one_hot_labels, (-1, image_size, image_size, num_classes)
+        )
+
+        # Sample random points in the latent space and concatenate the labels.
+        # This is for the generator.
+        batch_size = tf.shape(real_images)[0]
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+        random_vector_labels = tf.concat(
+            [random_latent_vectors, one_hot_labels], axis=1
+        )
+
+        # Decode the noise (guided by labels) to fake images.
+        generated_images = self.generator(random_vector_labels)
+
+        # Combine them with real images. Note that we are concatenating the labels
+        # with these images here.
+        fake_image_and_labels = tf.concat([generated_images, image_one_hot_labels], -1)
+        real_image_and_labels = tf.concat([real_images, image_one_hot_labels], -1)
+        combined_images = tf.concat(
+            [fake_image_and_labels, real_image_and_labels], axis=0
+        )
+
+        # Assemble labels discriminating real from fake images.
+        labels = tf.concat(
+            [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
+        )
+
+        # Train the discriminator.
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(combined_images)
+            d_loss = self.loss_fn(labels, predictions)
+        grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
+        self.d_optimizer.apply_gradients(
+            zip(grads, self.discriminator.trainable_weights)
+        )
+
+        # Sample random points in the latent space.
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+        random_vector_labels = tf.concat(
+            [random_latent_vectors, one_hot_labels], axis=1
+        )
+
+        # Assemble labels that say "all real images".
+        misleading_labels = tf.zeros((batch_size, 1))
+
+        # Train the generator (note that we should *not* update the weights
+        # of the discriminator)!
+        with tf.GradientTape() as tape:
+            fake_images = self.generator(random_vector_labels)
+            fake_image_and_labels = tf.concat([fake_images, image_one_hot_labels], -1)
+            predictions = self.discriminator(fake_image_and_labels)
+            g_loss = self.loss_fn(misleading_labels, predictions)
+        grads = tape.gradient(g_loss, self.generator.trainable_weights)
+        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
+
+        # Monitor loss.
+        self.gen_loss_tracker.update_state(g_loss)
+        self.disc_loss_tracker.update_state(d_loss)
+        return {
+            "g_loss": self.gen_loss_tracker.result(),
+            "d_loss": self.disc_loss_tracker.result(),
+        }
+
+cond_gan = ConditionalGAN(
+    discriminator=discriminator, generator=generator, latent_dim=latent_dim
+)
+cond_gan.compile(
+    d_optimizer=tf.keras.optimizers.Adam(learning_rate=0.000004, beta_1=0.5, beta_2=0.999, epsilon=1e-08),
+    g_optimizer=tf.keras.optimizers.Adam(learning_rate=0.00001, beta_1=0.5, beta_2=0.999, epsilon=1e-08),
+    loss_fn=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+)
+
+cond_gan.fit(train_ds, epochs=50)
+
+# We first extract the trained generator from our Conditiona GAN.
+trained_gen = cond_gan.generator
+
+model_name=f'./mammographies_gan_{datetime.now().strftime("%d%m%Y_%H%M%S")}'
+
+trained_gen.save(model_name)
